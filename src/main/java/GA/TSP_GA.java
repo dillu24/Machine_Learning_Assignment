5b52b6f9315801bc83bf231d9ba002647e4beda3
Dylan Galea <dillu_galea@hotmail.com>
2018-03-27 16:28:52 +0200

    fixed fitness for random

--- src/main/java/GA/TSP_GA.java
+++ src/main/java/GA/TSP_GA.java
@@ -331,7 +331,7 @@ public class TSP_GA {
             for(int k=listOfRoutes.size();k<populationSize;k++){ //maintain the population size by adding random route to
                                                                  //preserve diversity.
                 GARoute random = generateRandomTour();
-                random.setFitnessScore(random.getRouteCost(g));
+                random.setFitnessScore(fitnessFunction(random));
                 fitnessScoresList.add(random.getFitnessScore());
                 listOfRoutes.add(random);
             }
